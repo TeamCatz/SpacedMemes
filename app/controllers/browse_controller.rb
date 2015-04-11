13e388fe63dffe8328d98a4d80160709e9af279f
Tim Mott <tim.mott@segmint.com>
2015-04-11 16:33:46 -0400

    generated controllers

--- app/controllers/browse_controller.rb
+++
@@ -1,2 +0,0 @@
-class BrowseController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'app/browse'
+
+  get 'app/create'
+
+  get 'app/share'
+
+  get 'browse/view'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
