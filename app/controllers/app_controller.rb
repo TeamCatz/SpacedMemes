042e55fbee9e511c2c223c3c5d920640dacd5030
Tim Mott <tim.mott@segmint.com>
2015-04-12 12:46:00 -0400

    initial hook up of browse and create

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,16 +2,19 @@ class AppController < ApplicationController
   def browse
     @tags = Tags.select(:tag, 'count(*)').all.group(:tag).order(count: :desc)
     @images = Images.all.order(created_at: :desc).limit(10)
+    if params[:tag] and params[:tag].length > 0 then
+      @base_images = Tags.where(tag: params[:tag]).limit 50
+    else
+      @base_images = Tags.all.limit 50
+    end
+
   end
 
   def create
+    @base_image = BaseImage.find(params[:id])
   end
 
   def share
   end
 
-  def byTag
-    tag = params[:tag]
-    @tags = Tags.where(tag: tag)
-  end
 end
