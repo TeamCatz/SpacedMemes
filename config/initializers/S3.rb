c659c600654d0282eaa07ef292a4e3ab0e439d1e
Tim Mott <tim.mott@segmint.com>
2015-04-12 15:35:21 -0400

    removed keys

--- config/initializers/S3.rb
+++ config/initializers/S3.rb
@@ -1,7 +1,7 @@
 #TODO fill in values
 S3_BUCKET_NAME = 'spaceapps2015'
-S3_AWS_ACCESS_KEY_ID = 'AKIAJ5RNZZGOCZQDUXBQ'
-S3_AWS_SECRET_ACCESS_KEY = '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'
+S3_AWS_ACCESS_KEY_ID = ''
+S3_AWS_SECRET_ACCESS_KEY = ''
 
 
 #Aws.config(:credentials =>{:access_key_id => S3_AWS_ACCESS_KEY_ID, :secret_access_key =>  S3_AWS_SECRET_ACCESS_KEY})
@@ -13,8 +13,5 @@ S3_CLIENT = Aws::S3::Resource.new(
 S3_BUCKET = S3_CLIENT.bucket S3_BUCKET_NAME
 
 S3_IMAGE_BASE = "https://s3-us-west-1.amazonaws.com/#{S3_BUCKET_NAME}"
-# unless S3_BUCKET.exists? then
-#   S3_CLIENT.buckets.create(S3_BUCKET_NAME, :acl => :public_read)
-# end
 
 
